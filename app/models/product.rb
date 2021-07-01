dc7cd55a4d2369a9dd96f012b439752bff559846
mitesh8899 <mitesh.logicalstreet@gmail.com>
2021-07-01 11:22:33 +0530

    second

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+end
